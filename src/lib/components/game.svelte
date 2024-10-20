908ba243c35adac8178f8478a3da68bea14e593d
Louis-Paul Bowman <louispaul@ariseindustries.net>
2024-10-20 19:02:29 -0400

    add logos and start game component

---
+++ src/lib/components/game.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { teams, type TeamIds } from "$lib/espnApi";
+    export let homeId : TeamIds
+    export let awayId : TeamIds
+
+    const homeName = teams[homeId]
+    const homeLogo = `/img/logos/png/${homeId}.png`
+
+    const awayName = teams[awayId]
+    const awayLogo = `/img/logos/png/${awayId}.png`
+</script>
+
+<p>{awayName} at {homeName}</p>
+<img src={awayLogo} alt="{awayName} logo">
+<img src={homeLogo} alt="{homeName} logo">
