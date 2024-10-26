62544e2811ced083cba039ec78289b6c16ef0786
Louis-Paul Bowman <louispaul@ariseindustries.net>
2024-10-26 11:45:06 -0400

    added drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+//dotenv sucks but sveltekit's /$env is not available outside of /src so do this workaround
+dotenv.config({ path: '.env.local' });
+
+const DATABASE_URI = process.env.DATABASE_URI;
+
+//using dotenv makes these string|undefined so need this workaround to satisfy Config
+if (!DATABASE_URI)
+	throw new Error('Missing DB credential environment variables. Set them in .env.');
+
+export default {
+	schema: './src/lib/db/schema/*',
+	out: './src/lib/db/migrations',
+	dialect: 'postgresql',
+	dbCredentials: { url: DATABASE_URI },
+	verbose: true,
+	strict: false
+} satisfies Config;
