b704decf8e195ed6bd423e73b5d016f29da596ef
Louis-Paul Bowman <louispaul@ariseindustries.net>
2024-11-15 15:17:05 -0500

    vercel tweaks

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/public.js
+++
@@ -1,6 +0,0 @@
-const PUBLIC_SUPABASE_URL = "https://notcywottgczignlfmbr.supabase.co";
-const PUBLIC_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vdGN5d290dGdjemlnbmxmbWJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkyNzg4ODEsImV4cCI6MjA0NDg1NDg4MX0.4shvKzq2SOWirxvZnanaNDnbsSJFwlOJSsAC8xPRLag";
-export {
-  PUBLIC_SUPABASE_URL as P,
-  PUBLIC_SUPABASE_ANON_KEY as a
-};
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BbQuhktz.js","_app/immutable/chunks/scheduler.hKD0eBq_.js","_app/immutable/chunks/index.CuVcAe-p.js","_app/immutable/chunks/entry.BrVQtyW_.js","_app/immutable/chunks/index.DZrsBfdN.js"];
-export const stylesheets = [];
-export const fonts = [];
